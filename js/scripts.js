adc53f0fd3bf761b6d5457ce181ac7403412ba2b
Drake Wilcox and Isaac Garnand <isaac@>
2020-01-21 16:03:03 -0800

    made webpage with form that takes 6 text items and lists them in all caps and alphabetical order

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function()  {
+  $("form#groceries").submit(function(event){
+    var blanks = ["#input1","#input2","#input3","#input4","#input5","#input6"];
+   
+    var newArray = [];
+
+    blanks.forEach(function(blank){
+      var userInput = $(blank).val();
+      newArray.push(userInput);
+    });
+   
+    newArray.sort();
+    
+    newArray.forEach(function(item){
+      $("ul").append("<li>"+item.toUpperCase()+"</li>");
+    });
+
+    event.preventDefault();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // var userInput = blanks.map(function(blank){
+    //   blank.sort();
+    //   blank.toUpperCase();
+      
+  
+    // });
+    // alert(userInput);
+
+    // // userInput.forEach(function(item) {
+    // //     $(".output").append("<li>" + item + "</li>");
+    // // });
+    // // $(".output").show();
+    // event.preventDefault();
+//   });
+// });
